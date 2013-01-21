1bcaa46d07dbf2dabf09be40df36d7a49e6719b8
Takuma Takemura <takuma.takemura0104@gmail.com>
2013-01-21 15:30:22 +0900

    Fase Commit

---
+++ ActivatorCreateInstance/ActivatorCreateInstance/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ActivatorCreateInstance
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click( object sender, EventArgs e )
+		{
+			System.Reflection.Assembly asm;
+			try
+			{
+				asm = System.Reflection.Assembly.LoadFrom( "DllSample.dll" );
+				Type t = asm.GetType( "DllSample.Class1" );
+				dynamic dm = Activator.CreateInstance( t );
+				dm.Method1();
+			}
+			catch ( Exception ex )
+			{
+				MessageBox.Show("アセンブリがありません。" + ex.Message );
+			}
+		}
+	}
+}
---
+++ DllSample/DllSample/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace DllSample
+{
+	public class Class1
+	{
+		/// <summary>
+		/// Instance生成時、Windows Formを呼び出します
+		/// </summary>
+		public Class1()
+		{
+			System.Windows.Forms.MessageBox.Show("Hello World");
+		}
+
+		/// <summary>
+		/// Methodコール時、Windows Formを呼び出します
+		/// </summary>
+		public void Method1()
+		{
+			System.Windows.Forms.MessageBox.Show( "Hello Method1" );
+		}
+
+	}
+}
